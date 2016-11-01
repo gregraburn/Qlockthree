04a5a3e8147c344d8d04acbe38f04754883477f6
bracci <manuel.bracher@gmail.com>
2016-11-01 07:11:02 +0100

    Config for LPD8806 RGBW XXL

--- Configuration.h
+++ Configuration.h
@@ -38,7 +38,7 @@
 // DCF Debuganzeige
 #define USE_EXT_MODE_DCF_DEBUG
 // Datum manuell einstellen
-#define USE_EXT_MODE_DATE_MANUALLY
+//#define USE_EXT_MODE_DATE_MANUALLY
 
 // Im Menu auswählbare Sprachen aktivieren.
 #define ENABLE_LANGUAGE_DE
@@ -63,7 +63,7 @@
 //#define USE_INDIVIDUAL_CATHODES
 
 // Beim Einschalten leuchten alle LEDs während 5s
-#define LED_TEST_INTRO_ENABLED
+//#define LED_TEST_INTRO_ENABLED
 
 /*
  * Alarmfunktion einschalten?
@@ -94,20 +94,20 @@
 /*
  * Welches Board wird benutzt? Wird aktuell nur für das Pinmapping von LPD8806 und Neopixel gebraucht
  */
-#define BOARD_DEFAULT
-//#define BOARD_CLT
+//#define BOARD_DEFAULT
+#define BOARD_CLT
 //#define BOARD_BBRTCAD
 //#define BOARD_AMBBRTCAD
 
 /*
  * Welcher LED-Treiber soll benutzt werden?
  */
-#define LED_DRIVER_DEFAULT
+//#define LED_DRIVER_DEFAULT
 // #define LED_DRIVER_UEBERPIXEL
 // #define LED_DRIVER_POWER_SHIFT_REGISTER
 // #define LED_DRIVER_NEOPIXEL
 // #define LED_DRIVER_DOTSTAR
-// #define LED_DRIVER_LPD8806
+ #define LED_DRIVER_LPD8806
 
 /*
  * wenn LED_DRIVER_DEFAULT dann normalerweise Hardware mit UDN, 
@@ -118,9 +118,9 @@
 #endif
 
 #if defined(LED_DRIVER_LPD8806) || defined(LED_DRIVER_NEOPIXEL)
-#define RGB_LEDS
-//#define RGBW_LEDS
-//#define MATRIX_XXL
+//#define RGB_LEDS
+#define RGBW_LEDS
+#define MATRIX_XXL
 #endif
 
 /*
@@ -144,11 +144,11 @@
 /*
  * Welche IR-Fernbedienung soll benutzt werden?
  */
-#define REMOTE_NO_REMOTE
+//#define REMOTE_NO_REMOTE
 //#define REMOTE_SPARKFUN
 //#define REMOTE_MOONCANDLES
 //#define REMOTE_LUNARTEC
-//#define REMOTE_CLT
+#define REMOTE_CLT
 
 // kann separat oder zusätzlich zu einer IR-Fernbedienung genutzt werden
 //#define REMOTE_BLUETOOTH
